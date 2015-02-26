f17829c578f1be23305bd176f31e5f4c8c58d3eb
asi1024 <akifumiimanishi@yahoo.co.jp>
2015-02-26 11:30:08 +0900

    Impl type substitution

--- src/Typing.hs
+++ src/Typing.hs
@@ -15,3 +15,15 @@ typing tyenv (Import _) tystate = (tystate, (tyenv, Undefty))
 
 tyExp :: TyEnv -> Expr -> State TyState (Subst, Ty)
 tyExp _ _ = return ([], TyInt)
+
+substType :: Subst -> Ty -> Ty
+substType _ TyInt = TyInt
+substType subst (TyVar a) =
+  case subst of
+    []         -> TyVar a
+    ((x,y):xs) -> substType xs (if a == x then y else TyVar a)
+substType subst (TyFun a b) = TyFun (substType subst a) (substType subst b)
+substType _ Undefty = Undefty
+
+eqsOfSubst :: Subst -> [(Ty, Ty)]
+eqsOfSubst = map (\(a, b) -> (TyVar a, b))
