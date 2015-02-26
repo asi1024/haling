0e6e33fb5b9493d9108398b2a9cde562ecb491c0
asi1024 <akifumiimanishi@yahoo.co.jp>
2015-02-27 07:43:54 +0900

    Update tests typingのテストを追加した共に，evalのテストもリファクタリングした．

--- test/TypingSpec.hs
+++ test/TypingSpec.hs
@@ -21,20 +21,44 @@ typingWithEnv e s (x:xs) = let (a, (b, _)) = tyStr x e s in typingWithEnv b a xs
 tyTest :: [String] -> String
 tyTest = typingWithEnv [] 0
 
+tyPrelude :: [String] -> String
+tyPrelude s = tyTest $ prelude ++ s
+
+prelude :: [String]
+prelude = ["data Bool = True | False"]
+
 spec :: Spec
 spec = do
   describe "Eval" $ do
     it "should type basic arithmetic operations" $ do
-      tyTest ["1 + 2"]    `shouldBe`  "Int"
-      tyTest ["-2 * 2"]   `shouldBe`  "Int"
+      tyTest ["1 + 2"]   `shouldBe` "Int"
+      tyTest ["-2 * 2"] `shouldBe`  "Int"
+      tyTest ["1 < 2"]  `shouldBe`  "Bool"
 
     it "should type lambda functions" $ do
       tyTest ["\\x -> x"]   `shouldBe` "a -> a"
       tyTest ["\\x y -> x"] `shouldBe` "a -> b -> a"
       tyTest ["\\x y -> y"] `shouldBe` "a -> b -> b"
-      tyTest ["(\\x -> x + 1) 2 + (\\x -> x + -1) 3"] `shouldBe` "Int"
-      tyTest ["\\f g x -> g (f x)"] `shouldBe` "(a -> b) -> (b -> c) -> a -> c"
-      tyTest ["\\x y z -> x z (y z)"] `shouldBe` "(a -> b -> c) -> (a -> b) -> a -> c"
-      tyTest ["\\f g x -> g (f x)"] `shouldBe` "(a -> b) -> (b -> c) -> a -> c"
-      tyTest ["\\f g x -> g (f x)"] `shouldBe` "(a -> b) -> (b -> c) -> a -> c"
-      tyTest ["\\f x -> f x"] `shouldBe` "(a -> b) -> a -> b"
+      tyTest ["(\\x -> x + 1) 2 + (\\x -> x + -1) 3"]
+        `shouldBe` "Int"
+      tyTest ["\\f g x -> g (f x)"]
+        `shouldBe` "(a -> b) -> (b -> c) -> a -> c"
+      tyTest ["\\x y z -> x z (y z)"]
+        `shouldBe` "(a -> b -> c) -> (a -> b) -> a -> c"
+      tyTest ["\\b x -> if x b then x else (\\x -> b)"]
+        `shouldBe` "Bool -> (Bool -> Bool) -> Bool -> Bool"
+      tyPrelude ["\\x -> if True then x else (if x then True else False)"]
+        `shouldBe` "Bool -> Bool"
+      tyTest ["\\x y -> if x then x else y"]
+        `shouldBe` "Bool -> Bool -> Bool"
+      tyTest ["\\n -> (\\x -> x (\\y -> y)) (\\f -> f n)"]
+        `shouldBe` "a -> a"
+      tyTest ["\\f x -> f x"]
+        `shouldBe` "(a -> b) -> a -> b"
+      tyTest ["\\x y -> x (y x)"]
+        `shouldBe` "(a -> b) -> ((a -> b) -> a) -> b"
+      tyTest ["\\x y -> x (y x) (y x)"]
+        `shouldBe` "(a -> a -> b) -> ((a -> a -> b) -> a) -> b"
+      tyTest ["\\x y z -> x (z x) (y (z x y))"]
+        `shouldBe` "(((a -> b) -> a) -> b -> c) -> (a -> b)"
+            ++ " -> ((((a -> b) -> a) -> b -> c) -> (a -> b) -> a) -> c"
