732efaf659ddff4957383735deec39427d62bae7
asi1024 <akifumiimanishi@yahoo.co.jp>
2015-02-26 17:43:13 +0900

    Impl unify

--- src/Typing.hs
+++ src/Typing.hs
@@ -27,3 +27,17 @@ substType _ Undefty = Undefty
 
 eqsOfSubst :: Subst -> [(Ty, Ty)]
 eqsOfSubst = map (\(a, b) -> (TyVar a, b))
+
+unify :: [(Ty, Ty)] -> Subst
+unify [] = []
+unify ((t1, t2) : xs) =
+  if t1 == t2 || t1 == Undefty || t2 == Undefty
+    then unify xs
+    else case (t1, t2) of
+           (TyVar x, _) -> if elem x $ freevarTy t2
+                             then error "Type error."
+                             else (x, t2) : (unify $ map (func [(x, t2)]) xs)
+           (_, TyVar _) -> unify ((t2, t1) : xs)
+           (TyFun t11 t12, TyFun t21 t22) -> unify $ (t11, t21) : (t12, t22) : xs
+           (_, _) -> error "Type error."
+  where func l (a, b) = (substType l a, substType l b)
